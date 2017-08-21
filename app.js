36331f63c6f0577bbb3da03ec2607ed9ce8a6c58
晓明 <18833897@qq.com>
2017-08-22 02:46:05 +0800

    add project

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var users = require('./routes/user');
+const topicRouter = require('./routes/topic');
 
 var app = express();
 
@@ -23,7 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/user', users);
+app.use('/topic', topicRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,5 +44,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
